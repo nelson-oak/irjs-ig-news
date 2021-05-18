d34b3d134347685506c9d58a87b2fca68524dccf
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-17 21:12:47 -0300

    Aula 'Página Home' (C3-M1-SM2)

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="" />
+        <img src="/images/logo.svg" alt="logo" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
