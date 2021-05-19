9aeffeffe95274d23c68810bcdd0694a207d264c
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-19 20:44:49 -0300

    Aula 'Ouvindo eventos do Stripe' (C3-M2-SM4)

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from 'stream'
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+// Prova que até o Diego da Rocketseat copia código...
+async function buffer(readable: Readable) {
+  const chunks = []
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    )
+  }
+
+  return Buffer.concat(chunks)
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
-  console.log('evento recebido')
+  if (request.method === 'POST') {
+    const buf = await buffer(request)
+    const secret = request.headers['stripe-signature']
 
-  response.status(200).json({ ok: true })
+    let event: Stripe.Event
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (err) {
+      return response.status(400).send(`Webhook error: ${err.message}`)
+    }
+
+    const { type } = event
+
+    if(relevantEvents.has(type)) {
+      console.log('evento recebido', event)
+    }
+
+    response.json({ received: true })
+  } else {
+    response.setHeader('Allow', 'POST')
+    response.status(405).end('Method not allowed')
+  }
 }
