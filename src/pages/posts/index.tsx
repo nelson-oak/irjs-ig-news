2b68a9cf71a756748a5a3096cb5808bc63f2711e
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-20 20:27:36 -0300

    Aula 'Página: Post' (C3-M3-SM2)

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
@@ -26,13 +27,15 @@ export default function Posts({ posts}: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              
-              <strong>{post.updatedAt}</strong>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                
+                <strong>{post.updatedAt}</strong>
 
-              <p>{post.excerpt}</p>
-            </a>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
