83cfb2df9449f6af28e98f0dbd0b42bbc87411ec
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-20 20:01:16 -0300

    Aula 'Componente: ActiveLink' (C3-M3-SM1)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app'
 import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import '../styles/global.scss'
-import { Header } from './components/Header'
+import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
